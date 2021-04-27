a9e84133fb092f30f55630e33478feabc71c76cd
PeterPierre7 <pierre.wadoux7@gmail.com>
2021-04-27 11:52:40 +0200

    ajout rectangle + déplacement

--- Jeu.cpp
+++ Jeu.cpp
@@ -1,4 +1,7 @@
 # include <iostream>
+#include<SFML/System.hpp>
+#include<SFML/Graphics.hpp>
+#include<SFML/Window.hpp>
 
 #include "Jeu.hpp"
 
@@ -6,6 +9,7 @@
 Jeu::Jeu(){
   this->initVariables();
   this->initFenetre();
+  this->initEntity();
 }
 
 //Accesseurs
@@ -13,43 +17,87 @@ const bool Jeu::getIsRunning() const{
   return this->_window->isOpen();
 }
 
+//Fonction private
+
+void Jeu::initEntity(){
+
+  this->ent1.setSize(sf::Vector2f(100.f,100.f)); //.f -> float sans être float 
+  this->ent1.setFillColor(sf::Color::Cyan); //On peut direct mettre la couleur c'est pas mal
+
+}
 // Fonctions private
 void Jeu::initVariables(){
   this-> _window = nullptr;
   this->_videoMode.height = 600;
   this->_videoMode.width = 800;
+
+
   this->_spaceuse = 0;
-  this->_couleurFond = sf::Color(255,255,255,255); //blanc
+  this->_couleurFond = sf::Color::White; //blanc
+
+  this->_posEnt = sf::Vector2f(0,0); // On init en haut a gauche
+  this-> _vitEnt = 5; // Vitesse
+
 }
 void Jeu::initFenetre(){
   this-> _window = new sf::RenderWindow(this->_videoMode, "Bomberman", sf::Style::Close | sf::Style::Titlebar);
+  this->_window->setFramerateLimit(144);
+
+
 }
 //Fonctions Public
 //mise à jour du jeu (logique du jeu)
 void Jeu::updateEvents(){
+
   while(this->_window->pollEvent(this->_ev)) {
-    switch (this->_ev.type) {
-      case sf::Event::Closed:
+    switch (this->_ev.type) { //On regarde l'envent en cours
+
+
+
+      case sf::Event::Closed: // Si c'est l fermeture on ferme
         this->_window->close();
         break;
-      case sf::Event::KeyPressed:
-        switch (_ev.key.code){
-          case sf::Keyboard::Space:
-          this->_spaceuse = (this->_spaceuse+1)%3;
-          std::cout<<this->_spaceuse<<std::endl;
-          switch(this->_spaceuse){
+
+
+      case sf::Event::KeyPressed: //Si c'est une pression clavier, on regarde la quelle 
+        switch (_ev.key.code){//On verif le code /!\ qu'un touche à la fois pas ouf mais ok pour jeux 2d
+
+          case sf::Keyboard::Space: // Si espace :
+          this->_spaceuse = (this->_spaceuse+1)%3; // On tourne parmis les 3 couleurs
+          switch(this->_spaceuse){ 
             case 0:
-              this->_couleurFond = sf::Color(0,0,255,255);
+              this->_couleurFond = sf::Color::Blue;
               break;
             case 1:
-              this->_couleurFond = sf::Color(255,0,0,255);
+              this->_couleurFond = sf::Color::Green;
               break;
             case 2:
-              this->_couleurFond = sf::Color(0,255,0,255);
+              this->_couleurFond = sf::Color::Red;
              break;
-            }  
+          } 
+          break;
+
+
+          // On init les fleches pour déplacer 
+         case sf::Keyboard::Left: 
+            this->ent1.setPosition(this->ent1.getPosition()+sf::Vector2f(- this->_vitEnt,0));  // Le repère image est déplacer on fait attention à mettre les vitesse dans le bon sens
+             break;
+         case sf::Keyboard::Right:
+            this->ent1.setPosition(this->ent1.getPosition()+sf::Vector2f(this->_vitEnt,0));
+             break;
+         case sf::Keyboard::Up:
+            this->ent1.setPosition(this->ent1.getPosition()+sf::Vector2f(0,- this->_vitEnt));
+             break;
+          case sf::Keyboard::Down:
+            this->ent1.setPosition(this->ent1.getPosition()+sf::Vector2f(0, this->_vitEnt));
+             break;
+
+
+          break;
+        }
       break;
-      }
+
+      
     }
   }
 }
@@ -60,6 +108,9 @@ void Jeu::update(){
 void Jeu::render(){
   this->_window->clear(this->_couleurFond);//RGB+transparence
   //dessin des objets
+  this->_window->draw(this->ent1); // On place l'element sur le plateau
+
+
 
   //affichage de la fenetre
   this->_window->display();
--- Jeu.hpp
+++ Jeu.hpp
@@ -11,11 +11,25 @@ class Jeu{
     sf::RenderWindow* _window;
     sf::VideoMode _videoMode;
     sf::Event _ev;
-    int _spaceuse;
-    sf::Color _couleurFond;
+
+
+
+    sf::RectangleShape ent1;
+
+
+
+    // Variable pour gestion d'event 
+    int _spaceuse;  //incrément pour changer de couleur avec la barre d'espace
+    sf::Color _couleurFond; //Stockage de la couleur de fond du jeu
+
+    sf::Vector2f _posEnt; //Position de la seul entité
+    float _vitEnt; // vitesse de déplacement de l'entité /!\ si on augmemente trop ca ne sera plus continue 
+
+
     // Fonctions privées (que pour l'initialisation)
     void initVariables();
     void initFenetre();
+    void initEntity(); // Pour séparer l'init des entitées et des fenêtres qui stock les entitées 
 
   public :
     //Constructeur, Destructeur
