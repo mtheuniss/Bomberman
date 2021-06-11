d4f3f8441051a29acd02b3175233323a4a435a5d
PeterPierre7 <pierre.wadoux7@gmail.com>
2021-06-11 18:51:37 +0200

    ajout texture mur cassable

--- MurCassable.cpp
+++ MurCassable.cpp
@@ -25,7 +25,13 @@ MurCassable::PowerUp getPowerUp (){
 void MurCassable::affichage(){
   this->_esthetique = new sf::RectangleShape();
   this->_esthetique->setSize(sf::Vector2f(72.f,72.f)); //.f -> float sans être float (carrés 72*72)
-  this->_esthetique->setFillColor(sf::Color(205, 133, 63,255)); //On peut direct mettre la couleur c'est pas mal
   this->_esthetique->setOutlineThickness(1);
-  this->_esthetique->setOutlineColor(sf::Color(170, 68, 0));
+  this->_esthetique->setOutlineColor(sf::Color(0, 128, 0));
+  if (!this->_texture.loadFromFile("Images/mur_cassable.png"))
+  {
+    std::cout<<"erreur lors de l'ouverture de l'image"<<std::endl;
+  }
+  this->_texture.setSmooth(true);//lissage de l'image
+  this->_esthetique->setTexture(&this->_texture);
+  this->_esthetique->setTextureRect(sf::IntRect(0, 0, 256, 256));
 }
